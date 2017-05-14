4d9cbf5da52b78d53b8c2416edffae8a244f33ef
dastanaron <flow199@yandex.ru>
2017-05-15 01:59:44 +0600

    Уже работоспособное начало, но еще даже не версия.

--- classes/ApiMethods.php
+++ classes/ApiMethods.php
@@ -26,15 +26,41 @@ class ApiMethods{
         return $this;
     }
 
-    public function APIExecute()
+    public function SendMessageUser($userid, $message)
+    {
+        $random_id = crc32($userid .$message);
+        $this->api .= 'messages.send?user_id='.$userid.'&peer_id='.$userid.'&message='.urlencode($message).''; //&random_id='.$random_id.'
+        return $this;
+    }
+
+    public function APIExecute($response = true)
     {
         if ($this->addAccessTocken() && $this->addVersionApi()) {
-            return json_decode(file_get_contents($this->api), true);
+
+            if ($response) {
+                return $this->CURLExec();
+            }
+            else {
+                return json_decode($this->CURLExec(), true);
+            }
+            //return $this->api;
         }
         else {
             $this->error = 'Ошибка запроса не добавлены обязательные свойства запроса';
             return false;
         }
+    }
+
+    public function getMessage($time_offset = 60)
+    {
+        $this->api .= 'messages.get?time_offset='.$time_offset.'';
+        return $this;
+    }
+
+    public function getUsers($users_id)
+    {
+        $this->api .= 'users.get?user_ids='.$users_id.'';
+        return $this;
     }
 
     /*
@@ -65,9 +91,49 @@ class ApiMethods{
         return true;
     }
 
+    protected function CURLExec()
+    {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $this->api,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                "cache-control: no-cache",
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            $this->error = $err;
+        } else {
+            return $response;
+        }
+    }
+
     public function getError()
     {
         return $this->error;
+    }
+
+    public function getAPI()
+    {
+        return $this->api;
+    }
+
+    public function ClearAPI() {
+        $this->api = 'https://api.vk.com/method/';
+        return $this;
     }
 
 }
--- index.php
+++ index.php
@@ -1,21 +1,48 @@
 <?php
 
+echo '===cron Start===' . PHP_EOL;
+
 require 'classes/ApiMethods.php';
 
 require 'config.php';
 
-$api = new ApiMethods($user_access_tocken);
+$kristinaID = 69046497;
 
-dump($api->AccountSetOnline()->APIExecute());
+$api = new ApiMethods($group_access_tocken);
+
+
+//dump($api->getMessage()->APIExecute(false));
+
+$message_array = $api->getMessage(120)->APIExecute(false);
+
+$api->ClearAPI();
+
+$array_smile= ['😂','😄','😁'];
+
+foreach ($message_array["response"]["items"] as $item_msg){
+
+    //dump($api->getUsers($item_msg["user_id"])->APIExecute(false));
+
+    $name = $api->getUsers($item_msg["user_id"])->APIExecute(false)["response"][0]['first_name'];
+
+   echo $name . ' Написал(а): ' .$item_msg["body"] . PHP_EOL;
+
+   if (preg_match('#😂|😄|😁#', $item_msg["body"])) {
+       $api->ClearAPI();
+
+       $rand_smile = $array_smile[mt_rand(0,2)];
+       dump($api->SendMessageUser($item_msg["user_id"], $rand_smile)->APIExecute());
+       echo 'Бот ответил: '.$rand_smile.'' .PHP_EOL;
+   }
+
+}
+
 
 if (!empty($api->getError())){
     dump($api->getError());
 }
 
-
-/*$api = 'https://api.vk.com/method/account.getProfileInfo?access_token='.$user_access_tocken.'&v=5.64';
-
-dump(json_decode(file_get_contents($api), true));*/
+echo '===cron End===' . PHP_EOL;
 
 function dump($data) {
     echo '<pre>';
